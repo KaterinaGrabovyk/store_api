b1b2fc48675f5b0c2f11534341b359cf073b5e3f
KaterinaGrabovyk <katya.grabovik@ukr.net>
2025-06-11 20:16:48 +0300

    patch for table

--- init/schema.sql
+++ init/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE products (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
-    price DECIMAL(10,2) NOT NULL,
+    price DECIMAL(10,2) UNSIGNED NOT NULL,
     amount int UNSIGNED  NOT NULL,
     in_stock BOOLEAN NOT NULL
 );
